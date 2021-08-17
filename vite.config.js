f30b43c2ed7b508c90dd52c94b4533887374bd4d
SUDOCS <admin@imhcl.cn>
2021-08-17 15:33:02 +0800

    chore: brotliCompress

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        viteCompression({
+            algorithm: 'brotliCompress',
+            ext: 'br',
+        }),
+    ],
 })
