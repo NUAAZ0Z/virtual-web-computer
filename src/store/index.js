62cc76d2df1c9a88338431f9cea08b6abfb01753
SUDOCS <admin@imhcl.cn>
2021-09-06 11:11:01 +0800

    feat: state persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
 import { createStore } from 'vuex'
 import apps from './modules/apps'
+import createPersistedState from 'vuex-persistedstate'
 import wallpapers from './modules/wallpapers'
+import { CURRENT_WALLPAPER } from './state.type'
+
+const persistedState = createPersistedState({
+    // 设置要持久化的Stata
+    paths: [
+        // 使用了模块，需要使用完整State路径
+        ...[CURRENT_WALLPAPER].map(val => `wallpapers.${val}`),
+    ],
+})
 
 const store = createStore({
     modules: {
         apps,
         wallpapers,
     },
+    plugins: [
+        persistedState,
+    ],
 })
 
 export default store
